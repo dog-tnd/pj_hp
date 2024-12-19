197026ec4ccd0242dc9002f7acdcf1f8cb178e26
Ryo <jukenohtani@gmail.com>
2024-12-19 21:50:55 +0900

    サイトURLとベースパスを更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "static",
-  site: "https://dog-tnd.github.io/pj_hp",
-  base: "/pj_hp/",
+  site: "http://tus-tnd.com/",
+  base: "",
 });
