62240f67a5c63b76589016c990b7937a6cc465f0
taku072002T <taku07200@gmail.com>
2025-04-03 21:36:25 +0900

    Added sitemap configurations for google crawler.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
   output: "static",
   site: "http://tus-tnd.com/",
   base: "",
