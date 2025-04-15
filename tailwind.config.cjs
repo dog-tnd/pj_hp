1c358c9fb3e126943c26ed43ab3d4a49b4dae323
Ryo <jukenohtani@gmail.com>
2025-04-15 19:42:43 +0900

    tailwind.config.cjsにmdスクリーンサイズを追加

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
+    screens: {
+      'md': '1280px',
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
